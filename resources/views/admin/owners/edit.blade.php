10fb256e0eb23179694397e0f49226850b176f62
Hiroto <hirotomyk8995@gmail.com>
2022-10-29 19:09:38 +0900

    edit, update

--- resources/views/admin/owners/edit.blade.php
+++ resources/views/admin/owners/edit.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('オーナー登録') }}
+            {{ __('オーナー情報編集') }}
         </h2>
     </x-slot>
 
@@ -12,12 +12,14 @@
                     <section class="text-gray-600 body-font relative">
                         <div class="container px-5 mx-auto">
                             <div class="flex flex-col text-center w-full mb-12">
-                                <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">オーナー登録
+                                <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">オーナー情報編集
                                 </h1>
                             </div>
                             <div class="lg:w-1/2 md:w-2/3 mx-auto">
                                 <x-auth-validation-errors class="mb-4" :errors="$errors" />
-                                <form action="{{ route('admin.owners.store') }}" method="POST">
+                                <form action="{{ route('admin.owners.update', ['owner' => $owner->id]) }}"
+                                    method="POST">
+                                    @method('PUT')
                                     @csrf
                                     <div class="-m-2">
                                         <div class="p-2 w-1/2 mx-auto">
@@ -25,7 +27,7 @@
                                                 <label for="name"
                                                     class="leading-7 text-sm text-gray-600">オーナー名</label>
                                                 <input type="text" id="name" name="name"
-                                                    value="{{ old('name') }}"
+                                                    value="{{ $owner->name }}"
                                                     class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                                                     required>
                                             </div>
@@ -35,7 +37,7 @@
                                                 <label for="email"
                                                     class="leading-7 text-sm text-gray-600">メールアドレス</label>
                                                 <input type="email" id="email" name="email"
-                                                    value="{{ old('email') }}"
+                                                    value="{{ $owner->email }}"
                                                     class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                                                     required>
                                             </div>
@@ -64,7 +66,7 @@
                                                 onclick="location.href='{{ route('admin.owners.index') }}'"
                                                 class="bg-gray-200 border-0 py-2 px-8 focus:outline-none hover:bg-gray-400 rounded text-lg">戻る</button>
                                             <button type="submit"
-                                                class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">登録する</button>
+                                                class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">更新する</button>
                                         </div>
                                     </div>
                                 </form>
