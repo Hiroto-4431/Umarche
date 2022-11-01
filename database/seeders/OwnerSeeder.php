9a63d44335e9d46caeaa12999b0d020b23fb830a
Hiroto <hirotomyk8995@gmail.com>
2022-11-01 12:53:15 +0900

    pagination

--- database/seeders/OwnerSeeder.php
+++ database/seeders/OwnerSeeder.php
@@ -35,6 +35,48 @@ class OwnerSeeder extends Seeder
                 'password' => Hash::make('test1234'),
                 'created_at' => '2022/10/01 14:00:00',
             ],
+            [
+                'name' => '石塚審判',
+                'email' => 'test4@test.com',
+                'password' => Hash::make('test1234'),
+                'created_at' => '2022/10/01 16:00:00',
+            ],
+            [
+                'name' => '鈴木バイダン',
+                'email' => 'test5@test.com',
+                'password' => Hash::make('test1234'),
+                'created_at' => '2022/10/01 18:00:00',
+            ],
+            [
+                'name' => '西田さおり',
+                'email' => 'test6@test.com',
+                'password' => Hash::make('test1234'),
+                'created_at' => '2022/10/01 20:00:00',
+            ],
+            [
+                'name' => '大川内聡',
+                'email' => 'test7@test.com',
+                'password' => Hash::make('test1234'),
+                'created_at' => '2022/10/02 10:00:00',
+            ],
+            [
+                'name' => '吉野裕介',
+                'email' => 'test8@test.com',
+                'password' => Hash::make('test1234'),
+                'created_at' => '2022/10/02 12:00:00',
+            ],
+            [
+                'name' => '松井ケムリ',
+                'email' => 'test9@test.com',
+                'password' => Hash::make('test1234'),
+                'created_at' => '2022/10/02 14:00:00',
+            ],
+            [
+                'name' => '中村フー',
+                'email' => 'test10@test.com',
+                'password' => Hash::make('test1234'),
+                'created_at' => '2022/10/02 16:00:00',
+            ],
         ]);
     }
 }
